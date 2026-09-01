c67960ac446234b453416fb91c78791556820b96
Noriks (via Claude) <info@noriks.com>
2026-09-01 12:43:51 +0200

    Kratki opis: manjsi razmik med alinejami (8 -> 3 px, tesnejsa vrstica)

--- wp-content/themes/noriks/template_parts/product-bottom/why-seal.php
+++ wp-content/themes/noriks/template_parts/product-bottom/why-seal.php
@@ -485,11 +485,11 @@ $ico_recyc = '<path d="M7 7l2-3 2 3"/><path d="M9 4v9H5l2.5 4"/><path d="M15 20h
 /* kratki opis proizvoda: zelene kvacice umjesto tockica */
 .woocommerce-product-details__short-description ul,
 .woocommerce div.product .woocommerce-product-details__short-description ul {
-  list-style: none !important; margin: 8px 0 14px !important; padding-left: 0 !important; }
+  list-style: none !important; margin: 6px 0 12px !important; padding-left: 0 !important; }
 .woocommerce-product-details__short-description ul li,
 .woocommerce div.product .woocommerce-product-details__short-description ul li {
   list-style: none !important; list-style-type: none !important; padding-left: 26px !important;
-  text-indent: -26px !important; margin-left: 0 !important; line-height: 1.55 !important; margin-bottom: 8px !important; }
+  text-indent: -26px !important; margin-left: 0 !important; line-height: 1.45 !important; margin-bottom: 3px !important; }
 .woocommerce-product-details__short-description ul li::marker { content: "" !important; }
 .woocommerce-product-details__short-description ul li::before { content: none !important; }
 .woocommerce-product-details__short-description .nsl-tick {
--- wp-content/themes/noriks/template_parts/product-bottom/why-sr.php
+++ wp-content/themes/noriks/template_parts/product-bottom/why-sr.php
@@ -218,11 +218,11 @@ $sr_ico = function( $d ) {
 /* kratki opis proizvoda: zelene kvacice umjesto tockica */
 .woocommerce-product-details__short-description ul,
 .woocommerce div.product .woocommerce-product-details__short-description ul {
-  list-style: none !important; margin: 8px 0 14px !important; padding-left: 0 !important; }
+  list-style: none !important; margin: 6px 0 12px !important; padding-left: 0 !important; }
 .woocommerce-product-details__short-description ul li,
 .woocommerce div.product .woocommerce-product-details__short-description ul li {
   list-style: none !important; list-style-type: none !important; padding-left: 26px !important;
-  text-indent: -26px !important; margin-left: 0 !important; line-height: 1.55 !important; margin-bottom: 8px !important; }
+  text-indent: -26px !important; margin-left: 0 !important; line-height: 1.45 !important; margin-bottom: 3px !important; }
 .woocommerce-product-details__short-description ul li::marker { content: "" !important; }
 .woocommerce-product-details__short-description ul li::before { content: none !important; }
 .woocommerce-product-details__short-description .nsr-tick {
